b3050d526172cc2360724f67c3678345235e88c3
Rupesh Pandey <rupeshpandey432@gmail.com>
2024-10-03 17:54:23 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,10 @@ import { BiodataListComponent } from './components/biodata-list/biodata-list.com
 const routes: Routes = [
   { path: '', redirectTo: '/biodata-form', pathMatch: 'full' },
   { path: 'biodata-form', component: BiodataFormComponent },
-  { path: 'biodata-list', component: BiodataListComponent }
+  { path: 'biodata-list', component: BiodataListComponent },
+  { path: '**', redirectTo: '/biodata-form' } // Wildcard route for a 404 page or fallback
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
